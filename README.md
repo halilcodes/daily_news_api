c06b901738e30d25d9a9f01433d7d183aa783cfd
Halil_Hasmer <halilpython@gmail.com>
2023-08-05 17:50:31 +0300

    write a topic specific email sender script

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Daily news alert (learning API)
+
+This app accesses news about a particular topic and send e-mail
---
+++ main.py
@@ -0,0 +1,43 @@
+import requests
+import keys
+from pprint import pprint
+import smtplib
+import os
+import ssl
+
+email = os.getenv("GMAIL")
+password = os.getenv("GMAIL_PASS")
+host = "smtp.gmail.com"
+port = 465
+context = ssl.create_default_context()
+
+topic = "tesla"
+url = f"https://newsapi.org/v2/everything?" \
+      f"q={topic}&" \
+      f"language=en&" \
+      f"from=2023-07-05&" \
+      f"sortBy=relevancy&" \
+      f"apiKey={keys.news_api_key}"
+
+response = requests.get(url)
+content = response.json()
+message = f"Subject: Today's news about {topic.capitalize()} \n"
+
+for article in content['articles'][:20]:
+    if article['source']['id']:
+        title = article['title']
+        description = article['description']
+        news_link = article['url']
+        if title and description:
+            message += title + "\n" + description + "\n" + news_link + "\n"*2
+
+
+pprint(content)
+# print(message.count("\n"))
+message = message.encode("utf-8")
+
+if __name__ == "__main__":
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(email, password)
+        server.sendmail(from_addr=email, to_addrs=email, msg=message)
